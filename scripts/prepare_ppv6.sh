3f2d88731b167c9474a00998ffb28b2e69943ccd
Phat K. Tran <tkphat21@apcs.fitus.edu.vn>
2025-05-26 20:56:07 +0700

    add configs for pretraining

--- scripts/prepare_ppv6.sh
+++ scripts/prepare_ppv6.sh
@@ -19,12 +19,4 @@ huggingface-cli download benvijamin/rec_paddle_v6_part3 \
     --local-dir ./train_data/rec \
     --token $HF_KEY
 
-# Move data to fit with configuration
-mv  ./train_data/rec/rec_paddle_v6_part1/train/* ./train_data/rec/train
-mv  ./train_data/rec/rec_paddle_v6_part1/val/* ./train_data/rec/val
-mv  ./train_data/rec/rec_paddle_v6_part2/train/* ./train_data/rec/train
-mv  ./train_data/rec/rec_paddle_v6_part2/val/* ./train_data/rec/val
-mv  ./train_data/rec/rec_paddle_v6_part3/train/* ./train_data/rec/train
-mv  ./train_data/rec/rec_paddle_v6_part3/val/* ./train_data/rec/val
-
 wandb login $WB_KEY
--- scripts/prepare_ppv7.sh
+++ scripts/prepare_ppv7.sh
@@ -14,13 +14,4 @@ huggingface-cli download benvijamin/rec_paddle_v7_part2 \
     --local-dir ./train_data/rec \
     --token $HF_KEY
 
-mkdir ./train_data/rec/train
-mkdir ./train_data/rec/val
-
-# Move data to fit with configuration
-mv  ./train_data/rec/rec_paddle_v7_part1/train/* ./train_data/rec/train
-mv  ./train_data/rec/rec_paddle_v7_part1/val/* ./train_data/rec/val
-mv  ./train_data/rec/rec_paddle_v7_part2/train/* ./train_data/rec/train
-mv  ./train_data/rec/rec_paddle_v7_part2/val/* ./train_data/rec/val
-
 wandb login $WB_KEY
