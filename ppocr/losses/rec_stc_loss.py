d026e12fbe5d03a749d3f0d2b16aa8083772d2f2
Phat K. Tran <tkphat21@apcs.fitus.edu.vn>
2025-06-22 13:42:35 +0700

    Update base model

--- ppocr/losses/rec_stc_loss.py
+++ ppocr/losses/rec_stc_loss.py
@@ -258,7 +258,7 @@ class STCLoss(nn.Layer):
         stc_inputs = stc_inputs.transpose((1, 0, 2))
         
         # Compute Star CTC loss
-        loss = STCLossFunction.apply(stc_inputs, stc_targets, prob, "none")
+        loss = STCLossFunction.apply(stc_inputs, stc_targets, prob, "mean")
         
         # Apply focal loss if enabled
         if self.use_focal_loss:
@@ -268,8 +268,6 @@ class STCLoss(nn.Layer):
             weight = paddle.subtract(paddle.to_tensor([1.0]), weight)
             weight = paddle.square(weight)
             loss = paddle.mean(paddle.multiply(individual_losses, weight))
-        else:
-            loss = paddle.to_tensor([loss])
         
         return {"loss": loss}
 
